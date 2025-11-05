15ed779dc7f1ed1e698e0960d51aed599fbadc7f
argearhart <argrt00@gmail.com>
2025-11-05 16:33:03 -0500

    Add dice rolling functionality and bonus scoring logic

--- obj/Debug/net9.0/TestProject.AssemblyInfo.cs
+++ obj/Debug/net9.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf7af3b9e53d3ef97a7c0f3f93698db207c91135")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+791c2c4a91ccc9129d86b90d2f67b437f760f8f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
